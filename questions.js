abaa28b7a88e382aeeb204d199aa0c67734508eb
andrew-marshall1 <43158531+andrew-marshall1@users.noreply.github.com>
2018-11-04 11:42:19 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,63 @@
+var questionBank =
+[
+  {
+    question: "Nile Kinnick is the only Iowa Hawkeye to win the Heisman Trophy.",
+    answers: ["True", "False", "No one knows"],
+    correct: "True"
+  },
+  {
+    question: "What are the other two schools Hayden Fry coached at?",
+    answers: ["SMU, North Texas", "Texas A&M, SMU", "North Texas, Arkansas"],
+    correct: "SMU, North Texas"
+  },
+  {
+    question: "hich team did Iowa defeat in the 1996 and 2001 Alamo Bowls?",
+    answers: ["Texas", "Texas Tech", "Missouri"],
+    correct: "Texas Tech"
+  },
+  {
+    question: "HWhich quarterback was runner-up for the Heisman Trophy in 2002?",
+    answers: ["Nate Stanley", "Chuck Long", "Brad Banks"],
+    correct: "Brad Banks"
+  },
+  {
+    question: "Who did Iowa defeat in the classic #1 vs. #2 game in 1985?",
+    answers: ["Penn State", "Michigan", "Ohio State"],
+    correct: "Michigan"
+  },
+  {
+    question: " In 2002, the Hawkeyes finished the season with 2 losses, one to USC in the Orange Bowl, and their only regular season loss came from which team?",
+    answers: ["Iowa State", "Michigan", "Penn State"],
+    correct: "Iowa State"
+  },
+  {
+    question: "Who plays Iowa for the Floyd of Rosedale every year?",
+    answers: ["Minnesota", "Michigan State", "Wisconsin"],
+    correct: "Minnesota"
+  },
+  {
+    question: "Legendary Iowa head coach Hayden Fry spent how many seasons at the helm of the Hawkeyes?",
+    answers: ["18", "19", "20"],
+    correct: "20"
+  },
+  {
+    question: "What Senior player shined on the opening kickoff of the 2003 Orange Bowl in Miami, while setting the Orange Bowl record for a 100 yard return?",
+    answers: ["CJ Jones.", "Ed Hinkel", "Fred Russell"],
+    correct: "CJ Jones"
+  },
+  {
+    question: "Who was Iowa's QB during the 2008 season?",
+    answers: ["Brad Banks", "Jake Christensen", "Ricky Stanzi"],
+    correct: "Ricky Stanzi"
+  },
+  {
+    question: "How many Hawkeyes got drafted in the 2003 NFL draft?",
+    answers: ["5", "7", "9"],
+    correct: "5"
+  },
+  {
+    question: "How many wins does Iowa have in the CY-Hawk Rivalry?",
+    answers: ["35", "40", "44"],
+    correct: "44"
+  },
+]
